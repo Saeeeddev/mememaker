721ace72815d4535af169686c2f078b688e5d64c
saeed <dolatisaeed6@gmail.com>
2026-08-10 21:19:26 +0330

    fixed the header

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -4,7 +4,7 @@ import { BottomNav } from './BottomNav'
 export function AppShell() {
   return (
     <div className="min-h-dvh bg-[#000000] text-white overflow-hidden flex flex-col relative">
-      <main className="flex-1 overflow-y-auto pb-[calc(7rem+env(safe-area-inset-bottom))] pt-[calc(3.5rem+env(safe-area-inset-top))] px-4 custom-scrollbar">
+      <main className="flex-1 overflow-y-auto pb-[calc(7rem+env(safe-area-inset-bottom))] pt-[calc(1rem+env(safe-area-inset-top))] px-4 custom-scrollbar">
         <div className="max-w-md mx-auto w-full min-h-full">
           <Outlet />
         </div>
